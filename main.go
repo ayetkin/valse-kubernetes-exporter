5a3832d3788a82b1dc51f2254d40a4de272cea63
Ali Yetkin <ali.yetkin@hepsiburada.com>
2022-01-13 22:52:42 +0300

    migrated repo from internal gitlab

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 Ali Yetkin info@aliyetkin.com
+Copyright © 2021 Ali Yetkin info@aliyetkin.com / github.com/ayetkin
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
